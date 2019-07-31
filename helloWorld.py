b6b269f1707cb9bddb060193d4e73624a255da56
Robin S <robinscreech4@gmail.com>
2019-07-31 21:10:50 +0100

    adding some number stuff to this file

--- helloWorld.py
+++ helloWorld.py
@@ -5,3 +5,23 @@ def sayGoodbye():
 	print('Goodbye')
 
 sayGoodbye()
+
+#basic math 
+10+1 #11
+10-1 #9
+10*2 #20
+10/2 #5.0
+
+#Remainder numbers
+17/3 #5.66666
+int(17/3) #5
+17//3 #5
+17%3 #2
+17/3 - 17//3 #0.66666
+2**3 #8
+
+#Function for math
+bill = 15.25
+tax = 7.25
+total = bill + (tax/100)*bill
+total
