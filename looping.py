37c8bffc12355e9adb271c6804d9a7e0da942308
Robin S <robinscreech4@gmail.com>
2019-08-11 19:55:21 +0100

    adding first looping file

---
+++ looping.py
@@ -0,0 +1,13 @@
+#test list
+#list = [1,2,3,4,5]
+#for item in list :
+#    print(item)
+#for item in list[1:3]:
+#    print(item)
+
+names = ['Me','Cori','Books','Things']
+
+for name in names[1:3]:
+    print(name, len(name))
+    if name == 'Me':
+        print('This is ME')
