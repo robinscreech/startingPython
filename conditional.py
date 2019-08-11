919eb92d340dd0337d604beda4350e02456d45eb
Robin Screech <robinscreech4@gmail.com>
2019-08-11 18:42:43 +0100

    Condidtional file

---
+++ conditional.py
@@ -0,0 +1,8 @@
+number = int(input('Enter a number : '))
+
+if number > 10:
+    print('The number is greater than ten')
+elif number < 10:
+    print('the number is less than ten')
+else:
+    print('the number is ten')
