7955c338156719336463fd945082fb91ea35332b
Robin Screech <robinscreech4@gmail.com>
2019-08-12 13:36:32 +0100

    Adding in some work with range - incrementing and decrementing the count

---
+++ range.py
@@ -0,0 +1,26 @@
+
+#range(10)
+
+#for val in range(10):
+    #print(val)
+
+
+#for val in range(2, 10):
+#    print(val)
+
+#for val in range(2,10, 2):
+#    print(val)
+
+
+# incrementing count on range stepping 2
+# names = ['Me','Stuff','Books','Things']
+#
+# for i in range(0, len(names), 2):
+#    print(i, names[i])
+
+
+#decrementing count on range using -1
+names = ['Me','Stuff','Books','Things']
+
+for i in range(len(names) - 1, -1, -1):
+   print(i, names[i])
