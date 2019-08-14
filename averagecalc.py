d71d19977909d07a47d314d4b8734fb09f904284
Robin S <robinscreech4@gmail.com>
2019-08-14 22:22:21 +0100

    review tomorrow

--- averagecalc.py
+++ averagecalc.py
@@ -1,16 +1,28 @@
-userInput = input('Enter a set of numbers: ')
+# userInput = input('Enter a set of numbers: ')
+#
+# def genAvg(setOfNumbers):
+#     sumOfNumber = 0
+#
+#     for value in userInput:
+#         sumOfNumber += value
+#         totalAverage = sumOfNumber / len(userInput)
+#
+#     print('The total sum is: ', sumOfNumber)
+#     print('The total avg is: ', totalAverage)
+#
+# genAvg(userInput)
 
-def genAvg(setOfNumbers):
-    sumOfNumber = 0
+numbers = []
+totalSum = 0
 
-    for value in userInput:
-        sumOfNumber += value
-        totalAverage = sumOfNumber / len(userInput)
+while (True):
+    val = input('Please add numbers or (press \'q\' when done) : ')
+    if val == 'q':
+        return false
+    else:
+        for number in numbers:
+            totalSum += numbers[number]
+            print ('Total', totalSum)        
+        numbers.append(int(val))
 
-    print('The total sum is: ', sumOfNumber)
-    print('The total avg is: ', totalAverage)
-
-genAvg(userInput)
-
-enterNumber = 1279.00 / 100 * 17
-print('The dis is {0:.2f}'.format(enterNumber))
+print(numbers)
