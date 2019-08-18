97b43d36bca2dcefa3d14d737a22ea1dc0499528
Robin S <robinscreech4@gmail.com>
2019-08-18 19:48:47 +0100

    adding in a word anayzer. using multitude of new shiny things

---
+++ sentenceAnalyzer.py
@@ -0,0 +1,24 @@
+sentence = input("Please enter a sentence : ")
+
+lower_sentence = sentence.lower()
+
+words = lower_sentence.split(" ")
+
+char_count = {}
+
+for word in words:
+        characters = list(word)
+
+        for char in characters:
+
+            if char in char_count:
+                char_count[char] += 1
+
+            else:
+                char_count[char] = 1
+
+sorted_keys = sorted(char_count.keys())
+
+for key in sorted_keys:
+    value = char_count[key]
+    print("Found: ", key, value, "times")
