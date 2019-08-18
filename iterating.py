ed501b387d9d089b5b2f5262e8de0efae4eb8f0a
Robin S <robinscreech4@gmail.com>
2019-08-18 17:38:14 +0100

    iterating with dictonarys and lists

---
+++ iterating.py
@@ -0,0 +1,13 @@
+dict = {'Derek': 30, "Katie": 27, "Bob" : 7, "Cat": 5}
+
+# for pair in dict:
+#     print(pair, dict[pair])
+
+for key, val in dict.items():
+    print(key, val)
+
+print()
+
+list = [1,2,3,4,5,6,7,8,9]
+for index, value in enumerate(list):
+    print(index, value)
