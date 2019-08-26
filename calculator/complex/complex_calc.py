4fb9d6bb9cf6d3368182f14b42f29b6dabf97796
Robin Screech <robinscreech4@gmail.com>
2019-08-26 14:56:28 +0100

    commiting packages

---
+++ calculator/complex/complex_calc.py
@@ -0,0 +1,2 @@
+def compute():
+    print('this is complex')
---
+++ calculator/simple/simple_calc.py
@@ -0,0 +1,8 @@
+def compute():
+    print("this is simple")
+
+#>>> import calculator.simple.simple_calc
+#>>> calculator.simple.simple_calc.compute()
+#>>> from calculator.simple.simple_calc import compute
+
+#only jump down to the most logical level
