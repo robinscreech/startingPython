99715549b54ec4686b6fcaf47294a9511213f61e
Robin Screech <robinscreech4@gmail.com>
2019-08-27 14:23:55 +0100

    adding class files and some from package lesson

---
+++ person.py
@@ -0,0 +1,16 @@
+class Person:
+
+  def __init__(self):
+    self.first = "Robin"
+    self.last = "Screech"
+    self.age = 30
+
+  def greet(self):
+    return "hello there"
+
+# from person import Person
+# me = Person()
+# me.first
+# me.second
+# me.age
+# me.greet()
