b67d469b7e5a1136de9ec1490c20776d3af3b1ad
Robin Screech <robinscreech@Robins-MacBook-Air.local>
2019-09-16 21:26:12 +0100

    moving on, im done.

--- advcalc.py
+++ advcalc.py
@@ -23,16 +23,24 @@ class Calc:
     while (True):
         if firstInput == False:
             getValue = input("Please enter your numbers (q to quit):")
+
+            if getValue == "quit":
+                break
+
             inputVal = getValue.split(" ")
-            print("Your sum is :" + str(doSum(int(inputVal[0]),int(inputVal[2]),inputVal[1])))
-            history.append(doSum(int(inputVal[0]),int(inputVal[2]),inputVal[1]))
+            result = doSum(int(inputVal[0]),int(inputVal[2]),inputVal[1])
+            history.append(result)
             print(history)
             firstInput = True
 
         else:
             secondGetValue = input("Do some math with the last number (q to quit):")
+
+            if secondGetValue == "quit":
+                break
+
             secondSplit = secondGetValue.split(" ")
-            result = doSum(history[-1], int(secondSplit[1]), str(secondSplit[0]))
+            result = doSum(history[-1], int(secondSplit[1]), secondSplit[0])
             history.append(result)
             print(history)
             print("Your sum is : " + str(result))
